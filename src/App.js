94e70707052ac7f8658cbe5d71be68f39ffaa462
Fatima <bossfatya@gmail.com>
2022-12-21 17:51:14 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Forum from "./components/Forum";
+import Box from "./components/Box";
+
+function App() {
+  return (
+    <div>
+    <Forum/>
+    <Box/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Forum.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Forum = () => {
+  return (
+    <div>
+    <input type="text"></input>
+    <input type="text"></input>
+    <input type="color"></input>
+    </div>
+  )
+}
+
+export default Forum
